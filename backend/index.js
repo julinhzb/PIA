100f8b533c1430f0a6558c142b57ffac79bd3ee3
Julia Barbosa <bjulinhacardoso@gmail.com>
2023-09-04 09:14:49 -0300

    Conexão SQL

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const app= express();
+app.use(express.json())
 app.listen(9000, () => console.log("OK"));
 
+const mysql = require('mysql2/promise')
+const connection = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: ''
+})
 
 app.get('/', (req,res) => {
     res.send ("testeeeee");
+})
+
+const getAllpessoas = async () =>{
+    const [query] = await connection
+    .execute('select * from TestePessoa.Pessoa');
+    return query;
+}
+
+app.get('/pessoa', async (req,res)=>{
+    const consulta = await getAllpessoas();
+    return res.status(200).json(consulta);
 })

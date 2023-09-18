1cf3af867d0d48e21fb96478ac6b0402bd487c55
Julia Barbosa <bjulinhacardoso@gmail.com>
2023-09-18 09:12:04 -0300

    index

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app= express();
-app.use(express.json())
-app.listen(9000, () => console.log("OK"));
-
+const express=require('express')
+const app = express();
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+​
+app.listen(666, () => console. log("Aplicao respondendo na porta 666."));
+​
 const mysql = require('mysql2/promise')
 const connection = mysql.createPool({
     host: 'localhost',
@@ -10,18 +12,39 @@ const connection = mysql.createPool({
     user: 'root',
     password: ''
 })
-
-app.get('/', (req,res) => {
-    res.send ("testeeeee");
+​
+app.get('/',(req,res)=>{
+    res.send("Anderson");
 })
-
-const getAllpessoas = async () =>{
+​
+const getAllPessoas = async () =>{
     const [query] = await connection
     .execute('select * from TestePessoa.Pessoa');
     return query;
 }
-
 app.get('/pessoa', async (req,res)=>{
-    const consulta = await getAllpessoas();
+    const consulta = await getAllPessoas();
     return res.status(200).json(consulta);
+})
+​
+app.get('/pessoa/:id', async (req,res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?', [id]);
+    if(query.length === 0) return res.status(400).json({mensagem: 'Nao encontrado.'})
+    return res.status(200).json(query);
+})
+​
+app.get('/pessoa/busca/:nome', async (req,res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?', [id]);
+    if(query.length === 0) return res.status(400).json({mensagem: 'Nao encontrado.'})
+    return res.status(200).json(query);
+})
+​
+app.post('/pessoa', async (req,res)=>{
+    const {nome, email} = req.body;
+    const [query]= await connection.
+    execute('insert into TestePessoa.Pessoa (nome,email) values(?,?)', 
+    [nome,email])
+    return res.status(200).json(query);
 })
